4d6f3cb56373248d17724bd99e1eb2fb3de64e4a
Farah Alhaddad <120371923+Farahalh@users.noreply.github.com>
2024-01-20 21:06:19 +0100

    Adding routing to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './styles/App.css';
 import WelcomeScreen from './components/WelcomeScreen';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='flex items-center justify-center min-h-screen'>
-      <div className='bg-white shadow-md p-8 rounded-md'>
-        <WelcomeScreen />
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<WelcomeScreen />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
