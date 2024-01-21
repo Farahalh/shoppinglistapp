837e78479195506161eda65df7dbabe8e6af603d
Farah Alhaddad <120371923+Farahalh@users.noreply.github.com>
2024-01-21 15:56:02 +0100

    Changing UI to simplify, removing components and cleaning code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,8 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="text-black p-4 mb-6 flex justify-between">
-      <p className="mx-2">My Tasks</p>
-      <p className="mx-2">Shopping List</p>
-      <p className="mx-2">+New List</p>
+    <div className="Header text-black p-2 flex justify-between">
+      <h1 className="text-2xl font-bold">Shopping List App</h1>
     </div>
   );
 }
--- src/components/WelcomeScreen.tsx
+++ src/components/WelcomeScreen.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import EmptyStateImg from '../images/emptystate-stevengaan.png';
-import ItemPage from './ItemPage';
+import ItemListPage from './ItemListPage';
 import Header from './Header';
 
 function WelcomeScreen() {
     return (
-      <div className="flex flex-col items-center justify-center min-h-screen text-center border border-gray-300 p-8">
+      <div className="WelcomeScreen flex flex-col items-center justify-center min-h-screen text-center border border-gray-300 p-9">
         <Header />
+        <ItemListPage />
+        <h4 className='text-lg p-1'>No List Yet</h4>
+        <p className='text-sm p-1'>Create Your List Here</p>
         <img className='w-40 md:w-56 lg:w-72 max-w-full h-auto mb-5' src={EmptyStateImg} alt='emptyState'/>
-        <h4 className='text-lg'>No List Yet</h4>
-        <p className='text-sm'>Create Your List Here</p>
-        <ItemPage />
       </div>
     );
   }
